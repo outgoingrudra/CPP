dc05623a839cc989c0c99f8bdd6b1c583acf7d8d
Rudra Verma <rudraverma781@gmail.com>
2025-08-06 21:47:12 +0530

    Add files via upload

---
+++ swap2.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a = 10;
+    int b = 20;
+    
+    a= a+b;
+    b= a-b;
+    a =a-b;
+    
+    cout<<a<<","<<b<<endl;
+    
+
+    return 0;
+}
