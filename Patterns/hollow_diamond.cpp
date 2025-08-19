6022b45476b55bedc50d7e4d4b3be3459d39dae2
Rudra Verma <rudraverma781@gmail.com>
2025-08-19 06:21:44 +0530

    pattern

---
+++ Patterns/hollow_diamond.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) cout << " ";
+        cout << "*";
+        for (int j = 1; j < 2*i-1; j++) cout << " ";
+        if (i > 1) cout << "*";
+        cout << endl;
+    }
+    for (int i = n-1; i >= 1; i--) {
+        for (int j = i; j < n; j++) cout << " ";
+        cout << "*";
+        for (int j = 1; j < 2*i-1; j++) cout << " ";
+        if (i > 1) cout << "*";
+        cout << endl;
+    }
+}
