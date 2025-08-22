30075089d1394e13921512340db1327229770c10
Rudra Verma <rudraverma781@gmail.com>
2025-08-22 06:22:26 +0530

    work

---
+++ vector/freq.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+int main() {
+    vector<int> v={1,2,2,3,3,3,4};
+    map<int,int> freq;
+    for(int x:v) freq[x]++;
+    for(auto p:freq) cout<<p.first<<":"<<p.second<<endl;
+}
