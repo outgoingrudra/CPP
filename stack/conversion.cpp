22f00ab8b66640003a444a5bdeade63a8e23c0ae
Rudra Verma <rudraverma781@gmail.com>
2025-08-23 06:27:08 +0530

    work

---
+++ stack/conversion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int prec(char c){ if(c=='+'||c=='-') return 1; if(c=='*'||c=='/') return 2; return 0; }
+string infixToPostfix(string s){
+    stack<char> st; string res="";
+    for(char c:s){
+        if(isalnum(c)) res+=c;
+        else if(c=='(') st.push(c);
+        else if(c==')'){ while(!st.empty()&&st.top()!='('){ res+=st.top(); st.pop(); } st.pop(); }
+        else{ while(!st.empty()&&prec(st.top())>=prec(c)){ res+=st.top(); st.pop(); } st.push(c); }
+    }
+    while(!st.empty()){ res+=st.top(); st.pop(); }
+    return res;
+}
+int main(){ cout<<infixToPostfix("a+b*c"); }
