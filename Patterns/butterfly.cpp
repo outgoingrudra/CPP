c8c63415b876c5b3cb470fbd4efcb45cfbe83fef
Rudra Verma <rudraverma781@gmail.com>
2026-02-07 17:58:05 +0530

    Add butterfly pattern printing in C++

--- Patterns/butterfly.cpp
+++ Patterns/butterfly.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() {
     int n; cin >> n;
+    
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) cout << "*";
         for (int j = 1; j <= 2*(n-i); j++) cout << " ";
@@ -12,6 +13,9 @@ int main() {
         for (int j = 1; j <= i; j++) cout << "*";
         for (int j = 1; j <= 2*(n-i); j++) cout << " ";
         for (int j = 1; j <= i; j++) cout << "*";
+        
         cout << endl;
+        
     }
+    
 }
